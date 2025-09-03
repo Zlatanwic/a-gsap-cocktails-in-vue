ca6c393039cf5018ee4248d99e4653c5caeb8c28
Zlatanwic <2353113@tongji.edu.cn>
2025-09-03 21:29:01 +0800

    add the navbar function

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
+    <main>
+        <Navbar/>
+    </main>
     
-    <div class="flex-center h-[100vh]">
-        <h1 class="text-3xl text-indigo-300">hello gsap</h1>
-
-    </div>
 
 </template>
 
 <script setup>
 import { onMounted } from 'vue'
 import { ScrollTrigger ,SplitText } from 'gsap/all';
+import Navbar from './components/Navbar.vue'
 
 </script>
 
