a081eb728b727cba4533cb29666cac342665f0e5
Zlatanwic <2353113@tongji.edu.cn>
2025-09-04 14:43:42 +0800

    add the video animation

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -25,17 +25,31 @@
             </div>
         </div>
     </section>
+
+    <div class="video absolute inset-0">
+        <video ref="videoRef" src="/videos/output.mp4" muted playsinline preload="auto"></video>
+
+        
+    </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
+
+import { useMediaQuery } from '@vueuse/core';
 import {SplitText,ScrollTrigger} from 'gsap/all';
 import gsap from 'gsap';
 gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const videoRef=ref(null);
+const isMobile = useMediaQuery('(max-width: 767px)');
+
 onMounted(()=>{
     const heroSplit=new SplitText('.title',{type:'chars,words'});
 
     const paragraphSplit=new SplitText('.subtitle',{type:'lines'});
+
+    
 
     heroSplit.chars.forEach((char)=>char.classList.add('text-gradient'))
     gsap.from(heroSplit.chars,{
@@ -62,6 +76,29 @@ onMounted(()=>{
     })
     .to(".right-leaf", { y: 400 }, 0)
 	.to(".left-leaf", { y: -400 }, 0)
+
+    const startValue=isMobile.value?'top 50%':'center 60%';
+    const endValue=isMobile.value?'120% top':'bottom top';
+
+    if (videoRef.value) {
+      
+        const videoTl = gsap.timeline({
+          scrollTrigger: {
+            trigger: 'video', // 同样由 #hero 区域触发
+            start: startValue,
+            end: endValue,
+            scrub: true,
+            pin:true
+            // markers: true // 开发时可以取消注释来调试
+          }
+        });
+        videoRef.value.onloadedmetadata = () => {
+        // 3. 动画的目标是视频的 currentTime 属性
+        videoTl.to(videoRef.value, {
+          currentTime: videoRef.value.duration
+        });
+      };
+    }
 }
 )
 
