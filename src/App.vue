2e616db9776e7bd542780b063c718102d66d29d4
Zlatanwic <2353113@tongji.edu.cn>
2025-09-04 17:44:24 +0800

    add art,but come to a big react to vue problem and finally deal with it

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
         <cockTails/>
         <!-- <div class="h-dvh bg-black"></div> -->
         <About/>
+        <Art/>
     </main>
     
 
@@ -19,6 +20,7 @@ import Navbar from './components/Navbar.vue'
 import Hero from './components/Hero.vue'
 import cockTails from './components/cockTails.vue';
 import About from './components/About.vue';
+import Art from './components/Art.vue';
 
 
 </script>
--- src/constants/index.js
+++ src/constants/index.js
@@ -71,4 +71,18 @@ const mocktailLists = [
  },
 ];
 
-export {navLinks, cocktailLists, mocktailLists};
+const featureLists = [
+ "Perfectly balanced blends",
+ "Garnished to perfection",
+ "Ice-cold every time",
+ "Expertly shaken & stirred",
+];
+
+const goodLists = [
+ "Handpicked ingredients",
+ "Signature techniques",
+ "Bartending artistry in action",
+ "Freshly muddled flavors",
+];
+
+export {navLinks, cocktailLists, mocktailLists, featureLists, goodLists};
